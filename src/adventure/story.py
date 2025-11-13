cf6129f0d27abd3a2cc1454acc5ba6fd6bd4d3c9
Ava Ryan-Young <232468483+AvaRyan-Young@users.noreply.github.com>
2025-11-13 03:48:43 +0000

    changes in colors to story.py

--- src/adventure/story.py
+++ src/adventure/story.py
@@ -1,5 +1,12 @@
 from adventure.utils import read_events_from_file
 import random
+from rich.console import Console
+from rich.theme import Theme
+from rich.style import Style
+from rich import print
+from rich.prompt import Prompt
+
+console = Console()
 
 def step(choice: str, events):
     random_event = random.choice(events)
@@ -9,21 +16,21 @@ def step(choice: str, events):
     elif choice == "right":
         return right_path(random_event)
     else:
-        return "You stand still, unsure what to do. The forest swallows you."
+        return "[red]You stand still, unsure what to do. The forest swallows you.[/red]"
 
 def left_path(event):
-    return "You walk left. " + event
+    return "[cyan]You chose left. [/cyan]" + event
 
 def right_path(event):
-    return "You walk right. " + event
+    return "[magenta]You walk right. [/magenta]" + event
 
 if __name__ == "__main__":
     events = read_events_from_file('events.txt')
 
-    print("You wake up in a dark forest. You can go left or right.")
+    console.print("[i]You wake up in a dark forest.[/i] You can go [cyan]left[/cyan] or [magenta]right.[/magenta]")
     while True:
-        choice = input("Which direction do you choose? (left/right/exit): ")
-        choice = choice.strip().lower()
+        choice = Prompt.ask("Which direction do you choose? ([cyan]left[/cyan]/[magenta]right[/magenta]/[red]exit[/red])", case_sensitive=False)
+        choice = choice.strip()
         if choice == 'exit':
             break
         
